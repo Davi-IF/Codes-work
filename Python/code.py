321ec319fdf780ab2378619bf03a80e974172923
Davi_Matheus <dmnm2@aluno.ifal.edu.br>
2024-11-26 10:31:37 -0300

    Udpdate code 01

--- Python/code.py
+++ Python/code.py
@@ -1,5 +1,8 @@
-a = 2
-b = 3
-soma = a * b
 
-print(soma)
+wage = float(input("write your salary: "))
+percent = float(input("write the percent increase: "))
+increase = wage * (percent/100) 
+
+new_salary = increase + wage
+print(" new salary is:"+str(new_salary))
+
