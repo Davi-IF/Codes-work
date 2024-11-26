bbd2fc8d19c47f853a0d3395141779f277d345fe
Davi_Matheus <dmnm2@aluno.ifal.edu.br>
2024-11-26 12:44:04 -0300

    update code 2

--- Python/cod02.py
+++ Python/cod02.py
@@ -5,4 +5,4 @@ discount = float(input("type the discount"))
 tot_discount = price * (discount/100)
 final_value = price - tot_discount
 
-print("the final value:" +str(final_value))
+print(f"the final value: {final_value}" )
