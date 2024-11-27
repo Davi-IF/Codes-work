8d2d9e396d4abd1db3cb856dd36c3fb0bfac814b
Davi_Matheus <dmnm2@aluno.ifal.edu.br>
2024-11-27 10:55:10 -0300

    created cod4.py(LP)

---
+++ Python/cod4.py
@@ -0,0 +1,7 @@
+
+qtd_days = int(input("what was the amount of days:"))
+km_per = float(input(" What was the amount traveled km of the car: "))
+
+final_price = (qtd_days * 60) + (km_per * 0.15)
+
+print(f"The final price is:{final_price}")
