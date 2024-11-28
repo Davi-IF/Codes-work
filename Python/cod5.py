c97523aabff9bd31542f69c202d78c8f3c721da4
Davi_Matheus <dmnm2@aluno.ifal.edu.br>
2024-11-28 11:56:14 -0300

    up archives strangers

--- Python/cod5.py
+++ Python/cod5.py
@@ -1,7 +1,6 @@
 cig_day = int(input(" Type how many cigarettes for day: "))
 years = int(input("Type how many years: " ))
 
-
 qtd_days = years * 365
 tot_cig = qtd_days * cig_day
 lost_minutes = tot_cig * 10
