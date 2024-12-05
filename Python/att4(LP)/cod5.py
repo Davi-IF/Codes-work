934d8a34584ce391b66e291d04f33ef000c95649
Davi_Matheus <dmnm2@aluno.ifal.edu.br>
2024-12-04 23:54:57 -0300

    update

--- Python/att4(LP)/cod5.py
+++ Python/att4(LP)/cod5.py
@@ -3,17 +3,17 @@ def main():
     kwh = float(input("Enter the amount of kWh consumed: "))
     type = input("Enter the type of installation (R for Residential, C for Commercial, I for Industrial): ").upper()
 
-    if type == 'R':  # Residencial
+    if type == 'R': 
         if kwh <= 500:
             price = kwh * 0.40
         else:
             price = kwh * 0.65
-    elif type == 'C':  # Comercial
+    elif type == 'C': 
         if kwh <= 1000:
             price = kwh * 0.55
         else:
             price = kwh * 0.60
-    elif type == 'I':  # Industrial
+    elif type == 'I':  
         if kwh <= 5000:
             price = kwh * 0.55
         else:
@@ -22,9 +22,8 @@ def main():
         print("Invalid installation type!")
         return  
 
-    # Saída do resultado
+    
     print(f"The total amount to pay is: R$ {price:.2f}")
 
-# Chamada da função principal
 if __name__ == "__main__":
     main()
