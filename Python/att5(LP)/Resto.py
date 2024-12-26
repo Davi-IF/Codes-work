87ac625ca3f61af8932dc2008bc9b362d7c72188
Davi_Matheus <dmnm2@aluno.ifal.edu.br>
2024-12-26 16:32:06 -0300

    UP

--- Python/att5(LP)/Resto.py
+++ Python/att5(LP)/Resto.py
@@ -1,19 +1,19 @@
-# Solicitar ao usuário que insira os dois números
+
 dividend = int(input("Digite o dividendo: "))
 divisor = int(input("Digite o divisor: "))
-
-
+ 
+ 
+ 
 if divisor == 0:
-    print("Erro: Divisão por zero não é permitida.")
+        print("Erro: Divisão por zero não é permitida.")
 else:
-    quotient = 0
-    remainder = dividend
+        quotient = 0
+        remainder = dividend
 
-  
-    while remainder >= divisor:
-        remainder -= divisor  
-        quotient += 1          
+        while remainder >= divisor:
+            remainder -= divisor  
+            quotient += 1          
 
 
-    print(f"O quociente é: {quotient}")
-    print(f"O resto da divisão é: {remainder}")
+        print(f"O quociente é: {quotient}")
+        print(f"O resto da divisão é: {remainder}")
