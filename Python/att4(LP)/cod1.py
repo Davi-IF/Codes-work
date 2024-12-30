8e5f85b2e075cbd7616292102d7358825c018b29
Davi_Matheus <dmnm2@aluno.ifal.edu.br>
2024-12-30 19:56:24 -0300

    up

--- Python/att4(LP)/cod1.py
+++ Python/att4(LP)/cod1.py
@@ -1,6 +1,6 @@
-a = int(input("type value of the a:"))
+a = int(input("type value of the a:")) 
 b = int(input("type value of the b:"))
-c = int(input("type value of the c:"))
+c = int(input("type value of the c:")) 
 
 biggest = max(a, b ,c)
 smallest = min(a, b, c)
